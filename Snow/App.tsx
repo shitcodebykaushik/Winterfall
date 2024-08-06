1a54e22f2ee6599207e13eea75b3b289bd714804
Kaushik raj <algorithmunloack@gmail.com>
2024-08-06 09:57:15 +0530

    The state is done here :

--- Snow/App.tsx
+++ Snow/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, Text} from 'react-native';
 
-const HelloWorldApp = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, Kaushik!</Text>
-    </View>
-  );
-};
-export default HelloWorldApp;
+// This is the react class component named with the Mycomponent  which uses state to manage the data.
+class MyComponent extends React.Component {
+  uwuw = {
+    year: 2016,
+    name: 'Kaushik',
+    colors: ['Red'],
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>My name is: {this.uwuw.name}</Text>
+        <Text>The year is: {this.uwuw.year}</Text>
+        <Text>Favorite color is: {this.uwuw.colors[0]}</Text>
+      </View>
+    );
+  }
+}
+
+export default MyComponent;
