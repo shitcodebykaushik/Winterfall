35501bb143ab323caa9e7aa53857fcb788b30433
Kaushik raj <algorithmunloack@gmail.com>
2024-08-08 15:10:15 +0530

    The welcome file is running peacefully without any conflict

--- Snow/src/screens/Welcome.js
+++ Snow/src/screens/Welcome.js
@@ -5,13 +5,17 @@ const Welcome = () => {
   return (
     <View style = {styles.container}>
       <Image style={styles.patternbg}source= {pattern} />
+      {/*<Text style = {styles.head}>HI</Text>*/} 
+      <View style = {styles.container1}>
+        <Text style={styles.head}>Welcome to m app</Text>
+        </View>  
     </View>
   )
 }
 
 export default Welcome
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
     container: {
         width: '100%',
         height: '100%',
@@ -21,5 +25,15 @@ const styles = StyleSheet.create({
         top: 0,
         width: '100%',
         height: '100%',
+    },
+    head: {
+      fontSize: 30,
+      color: 'red'
+    },
+    container1: {
+      display: "flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%'
     }
 })
