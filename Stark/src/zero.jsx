779ba3e02fb255004d1464446b5a61709a96bf72
Kaushik raj <algorithmunloack@gmail.com>
2024-08-13 23:38:11 +0530

    The part one is done of the form :

--- Stark/src/zero.jsx
+++ Stark/src/zero.jsx
@@ -1,18 +1,70 @@
-import React from "react";
-import { Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert, Pressable } from "react-native";
 
-const mybutton= ()=>{
-  const Press = ()=>{
-  alert("Done")
+const App = ({ navigation }) => {
+  const [userDetails, setUserDetails] = useState({
+    username: "",
+    email: "",
+  });
 
+  const handlelogin = async () => {
+    try {
+      if (userDetails.username === "") {
+        throw new Error("Please fill all fields");
+      }
+      Alert.alert("Great Work", "Well done");
+    } catch (error) {
+      Alert.alert("Something went wrong", error.message);
+    }
+  };
+
+  return (
+    <View style={styles.loginPageContainer}>
+      <Text style={styles.title}>User Registration</Text>
+      
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          name="username"
+          value={userDetails.username}
+          onChangeText={(text) =>
+            setUserDetails((prev) => ({
+              ...prev,
+              username: text,
+            }))
+          }
+        />
+      </View>
+
+    
+
+      <Button title="Login" onPress={handlelogin} />
+    </View>
+  );
 };
-return (
-  <Button
-  title="Press  Me "
-  onPress={Press}  
-  
-  
-  />
-);
-};
-export default mybutton;
+
+const styles = StyleSheet.create({
+  loginPageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: "center",
+    color:"red"
+  },
+  inputContainer: {
+    marginBottom: 15,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+  },
+});
+
+export default App;
